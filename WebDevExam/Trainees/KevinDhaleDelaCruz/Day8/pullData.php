9994a34791fcf2949ddc4df710b8b7c2292f2acb
ksdelacruz <kevindhaledelacruz@gmail.com>
2016-02-09 00:59:33 +0800

    + 1.0 First versions of files for Day 8

---
+++ WebDevExam/Trainees/KevinDhaleDelaCruz/Day8/pullData.php
@@ -0,0 +1,84 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+<title>Table "accel"</title>
+	
+<link rel="stylesheet" type="text/css" href="mystyle.css">
+<script src="http://code.jquery.com/jquery-1.11.3.min.js"></script>
+<script src="ddtf.js"></script>
+
+
+</head>
+
+<body>
+
+<div id="header">
+	<h1 align="center">Table "accel"</h1>
+</div>
+
+<div class="container">
+
+	<div id="sidebar">
+		<div align="center">
+			<p>Sidebar</p>
+		</div>
+	</div>
+
+<?php
+
+//header('Content-type: text/plain');
+
+$server = "localhost";
+$username = "trainee";
+$password = "updews";
+$dbname = "updews_training";
+
+$con = new PDO("mysql:host=$server;dbname=$dbname", $username, $password);
+$con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+$stmt = $con->prepare("SELECT * FROM accel");
+$stmt->execute();
+
+?>
+
+
+	<div id="content">
+		<div id="tableDiv">
+			<table id="myTable" align="center">
+					<tr>
+						<th>Timestamp</th>
+						<th>Node</th>
+						<th>X-Val</th>
+						<th>Y-Val</th>
+						<th>Z-Val</th>
+						<th>M-Val</th>
+						<th>Purged</th>
+					</tr>
+					<?php
+					foreach( $stmt as $row )
+					{
+						echo "<tr>";
+					    echo "<td>".$row['time_stamp']."</td>";
+					    echo "<td>".$row['node']."</td>";
+					    echo "<td>".$row['xval']."</td>";
+					    echo "<td>".$row['yval']."</td>";
+					    echo "<td>".$row['zval']."</td>";
+					    echo "<td>".$row['mval']."</td>";
+					    echo "<td>".$row['purged']."</td>";
+					    echo "</tr>";
+					}
+					?>
+					</tr>
+			</table>
+			<script>
+				jQuery('#myTable').ddTableFilter();
+			</script>
+
+		</div>
+	</div>
+</div>
+	
+
+</body>
+</html>
