768178d45ef3f71f02995864fb7bf455e9857829
gailquijano <glaisa_quijano@yahoo.com>
2016-07-19 20:53:36 +0800

    Update navBar.php
    edit option value

--- updews-webdev-hiring/WebDevExam/Trainees/GlaisaQuijano/Day4/navBar.php
+++ updews-webdev-hiring/WebDevExam/Trainees/GlaisaQuijano/Day4/navBar.php
@@ -4,5 +4,8 @@
                     <a href="home.php">HOME</a>
                     <a href="findUs.php">FIND US</a>
                     <a href="about.php">ABOUT</a>
+                    <a href="Day3.php">Day3</a>
+                    <a href="day2.php">Day1 & Day2</a>
                 </div>
             </div>
+  
